12f7dc065fbdab403268ec1f681f9dff56f74b89
rosecip <rosecipriano10@gmail.com>
2022-06-10 17:56:23 -0400

    pigIt

---
+++ pigIt/pigIt.js
@@ -0,0 +1,8 @@
+const pigIt = (str) => {
+  let strArr = str.split(" ")
+  let finalArr = []
+  for (let i = 0; i < strArr.length; i++) {
+    finalArr.push(strArr[i].slice(1) + strArr[i].slice(0, 1) + "ay")
+  }
+  return finalArr.join(" ")
+}
---
+++ pigIt/pigIt.md
@@ -0,0 +1,3 @@
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+[source](https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript)
