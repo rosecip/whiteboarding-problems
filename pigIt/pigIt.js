636bfb47dcf6a5fccabfc56a1bbe0d464092dcca
rosecip <rosecipriano10@gmail.com>
2022-06-12 16:39:47 -0400

    pigIt

--- pigIt/pigIt.js
+++ pigIt/pigIt.js
@@ -4,8 +4,6 @@ const pigIt = (str) => {
   for (let i = 0; i < strArr.length; i++) {
     if (strArr[i].match(/[a-z]/i)) {
       finalArr.push(strArr[i].slice(1) + strArr[i].slice(0, 1) + "ay")
-    } else {
-      finalArr.push(strArr[i])
     }
   }
   return finalArr.join(" ")
